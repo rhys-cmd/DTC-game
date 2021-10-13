c759ecb4eaf9d4dc6897007fab660060155350e3
rhys-cmd <81196117+rhys-cmd@users.noreply.github.com>
2021-10-14 00:28:17 +1300

    tilemapsucess

--- Main.py
+++ Main.py
@@ -1,20 +1,19 @@
 import arcade
 
-
 GRAVITY = 1
 PLAYER_JUMP_SPEED = 10
 
 
 
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 900
 SCREEN_TITLE = "game"
 
 CHARACTER_SCALING = 1
 
 
 
-TILE_SCALING = 0.5
+TILE_SCALING = 1
 COIN_SCALING = 0.5
 
 class gameWindow(arcade.Window):
@@ -32,7 +31,7 @@ class gameWindow(arcade.Window):
         self.view_left = 0
 
 
-        
+        self.tile_map = None
 
     def setup(self):
         self.player_list = arcade.SpriteList()
@@ -49,81 +48,34 @@ class gameWindow(arcade.Window):
         self.player_sprite.left = False
         self.player_sprite.up = False
         self.player_sprite.down = False
-
-        # Create the ground
-        # This shows using a loop to place multiple sprites horizontally
-        for x in range(0, 1250, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
-            wall.center_x = x
-            wall.center_y = 32
-            self.wall_list.append(wall)
-        # Put some crates on the ground
-        # This shows using a coordinate list to place sprites
-        coordinate_list = [[512, 96],
-                            [256, 96],
-                            [768, 96]]
+ 
         
-        for coordinate in coordinate_list:
-            # Add a crate on the ground
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
-            wall.position = coordinate
-            self.wall_list.append(wall)
+        
+        map_name = "dtctilemap1.tmx"
+        platform_layer_name = "Tile Layer 1"
 
+        my_map =  arcade.tilemap.read_tmx(map_name)
+        self.wall_list = arcade.tilemap.process_layer(map_object=my_map,
+                                                      layer_name=platform_layer_name,
+                                                      scaling=TILE_SCALING,
+                                                      use_spatial_hash=True)
+        
+        
+    
+
+        
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
                                                              self.wall_list,
                                                              GRAVITY)
     
-    self.physics_engine.update
+        self.physics_engine.update
     
+     
     changed = False
 
-    #scroll left
-    left_boundary = self.view_left + SCREEN_WIDTH - LEFT_VIEWPORT_MARGIN
-    if self.player_sprite.right > right_boundary:
-        self.view_left -= left_boundary - right_boundary
-        changed = True
+   
 
-    #scroll right
-    right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
-    if self.player_sprite.right > right_boundary:
-        self.view_left -= left_boundary - right_boundary
-        changed = True
-
-    #scroll up
-    top_boundary = slef.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
-    if self.player_sprite.top > top_boundary:
-        self.view_bottom += self.player_sprite.top - top - top_boundary
-        changed  = True
-
-
-    #scroll down
-    bottom_boundary = self.veiw_bottom + BOTTOM_VIEWPORT_MARGIN
-    if self.player_sprite.bottom < bottom_boundary:
-        self.veiw_bottom -= bottom_boundary - self.player_sprite.bottom
-        changed = True
-
-
-    if changed:
-        self.veiw_bottom = int(self.veiw_bottom)
-        self.veiw_left = int(self.veiw_left)
-    
-    
-    arcade.set_viewport(self.veiw_left,
-                    SCREEN_WIDTH + self.veiw_left,
-                    self.view_bottom,
-                    SCREEN_HEIGHT + self.veiw_bottom )
-
-    LEFT_VIEWPORT_MARGIN = 250
-    RIGHT_VIEWPORT_MARGIN = 250
-    BOTTOM_VIEWPORT_MARGIN = 250
-    TOP_VIEWPORT_MARGIN = 100
-    
-    
-    
-    
-    
-    
-    
+   
     
     
     
@@ -135,6 +87,7 @@ class gameWindow(arcade.Window):
         self.player_list.draw()
         self.wall_list.draw()
         self.player_list.draw()
+    
     
     def update(self, delta_time):
         self.player_sprite.update()
@@ -164,7 +117,6 @@ class gameWindow(arcade.Window):
             self.player_sprite.change_y = 0  
 
     
-
 
 
 
---
+++ final tile map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="final tile map" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="C:/Users/Rhys/Downloads/final tile map.png" width="500" height="500"/>
+</tileset>
